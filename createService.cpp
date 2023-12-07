58089690294d26d3fcd24e647edc88bac5ebed92
ltx <2050365570@qq.com>
2023-12-07 19:47:47 +0800

    注册exe为windows服务

--- createService.cpp
+++ createService.cpp
@@ -6,8 +6,10 @@ void createService() {
     std::string serviceName;
     std::string path;
     std::string command = "sc create ";
+    // 例如: nginx
     std::cout << "enter the service name: ";
     std::cin >> serviceName;
+    // 例如: D:\tools\nginx-1.24.0\nginx.exe
     std::cout << "enter the exe file path: ";
     std::cin >> path;
     command.append(serviceName).append(" binPath= \"").append(path).append("\"");
